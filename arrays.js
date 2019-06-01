b1d37ff86bf861fb1e62a1a3e990fc9462bd1d9f
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 23:17:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 var string = "foo"
-function addElementToBeginningOfArray(chocolateBars,foo){ 
-  return [foo,...chocolateBars]
+function addElementToBeginningOfArray(chocolateBars,string){ 
+  return ["foo",...chocolateBars]
 }
 function destructivelyAddElementToBeginningOfArray()
