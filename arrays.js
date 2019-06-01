ec3f622604b2dd0850e5f468a9090f6cba0c19e8
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 23:38:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,5 @@ function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-  
+  chocolateBars.pop()
 }
