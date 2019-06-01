02937146b2b12f57182cb2a1a4fe39f7434ebf5b
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 23:27:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,9 +3,7 @@ var string = "foo"
 function addElementToBeginningOfArray(chocolateBars,string){ 
   return [string,...chocolateBars]
 }
-function destructivelyAddElementToBeginningOfArray(chocolateBars, chocolateBars.unshift(string)){
-  return chocolateBars
-}
+function destructivelyAddElementToBeginningOfArray(
 function addElementToEndOfArray(chocolateBars,string){
   return [...chocolateBars,string]
 }
