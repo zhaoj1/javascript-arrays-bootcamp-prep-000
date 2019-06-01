aa0c5bd1000274128ec5a58501fc93e22e28bd48
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 23:15:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
-function addElementToBeginningOfArray(chocolateBars,foo) = return{
-  ["foo",...chocolateBars]
+function addElementToBeginningOfArray(chocolateBars,foo){ 
+  return ["foo",...chocolateBars]
 }
